f6d246cca04bf1e5ac8b82e2cbf377ae6ff2ba67
Le Quoc Viet <vietlq85@gmail.com>
2013-05-28 05:51:07 +0800

    --

--- coffeemix/coffeemix/CoffeeMix.h
+++ coffeemix/coffeemix/CoffeeMix.h
@@ -130,7 +130,7 @@ namespace coffeemix
         template <typename U, U> struct type_check;                             \
         template <typename _1> static yes & chk(type_check<Sign, &_1::func> *); \
         template <typename   > static no  & chk(...);                           \
-        static bool const value = sizeof(chk<T>(0)) == sizeof(yes);             \
+        static bool const value = (sizeof(chk<T>(0)) == sizeof(yes));           \
     }
     
     HAS_MEM_FUNC(toStream, got_to_stream);
