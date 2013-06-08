917c5207ee77abb7ea51cdd31a39716e2771cf1b
Le Quoc Viet <vietlq85@gmail.com>
2013-06-09 07:39:34 +0800

    Refined CMakeLists.txt to show header files into a separate group in XCode

---
+++ WordPath/Dictionary.h
@@ -0,0 +1,14 @@
+//
+//  Dictionary.h
+//  WordPath
+//
+//  Created by Le Quoc Viet on 9/6/13.
+//
+//
+
+#ifndef __WordPath__Dictionary__
+#define __WordPath__Dictionary__
+
+#include <iostream>
+
+#endif /* defined(__WordPath__Dictionary__) */
---
+++ WordPath/build-make/CMakeFiles/word_path.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/word_path.dir/main.cpp.o"
+  "CMakeFiles/word_path.dir/Dictionary.cpp.o"
+  "word_path.pdb"
+  "word_path"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/word_path.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
