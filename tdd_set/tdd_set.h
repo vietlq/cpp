a8577182d3b56514cf2b74ab00f8ea38966d771a
Le Quoc Viet <vietlq85@gmail.com>
2013-06-08 20:56:49 +0800

    Added implementation of Set data structure with Google Test unit testing

---
+++ tdd_set/tdd_set.h
@@ -0,0 +1,71 @@
+#ifndef SET_H
+#define SET_H
+
+class Set
+{
+public:
+	Set(): _size(0), _pArr(0)
+	{
+		_pArr = new int[1024];
+	}
+	
+	~Set()
+	{
+		delete[] _pArr;
+	}
+	
+	bool isEmpty() const
+	{
+		return (_size < 1);
+	}
+	
+	void add(int value)
+	{
+		if(! contains(value))
+		{
+			_pArr[_size] = value;
+			++_size;
+		}
+	}
+	
+	int size() const
+	{
+		return _size;
+	}
+	
+	bool contains(int value) const
+	{
+		return (getIndexOf(value) > -1);
+	}
+	
+	void remove(int value)
+	{
+		int idx = getIndexOf(value);
+		
+		if(idx > -1)
+		{
+			if(idx < _size - 1)
+			{
+				_pArr[idx] = _pArr[_size - 1];
+			}
+			--_size;
+		}
+	}
+private:
+	int _size;
+	int * _pArr;
+	
+	int getIndexOf(int value) const
+	{
+		int idx = 0;
+		
+		while((idx < _size) && (_pArr[idx] != value))
+		{
+			++idx;
+		}
+		
+		return ((idx < _size) ? idx : -1);	
+	}
+};
+
+#endif // SET_H
---
+++ tdd_set/test.cpp
@@ -0,0 +1,117 @@
+#include <gtest/gtest.h>
+#include "tdd_set.h"
+
+class TestSet: public ::testing::Test {
+protected:
+	TestSet()
+	{
+		
+	}
+	
+	virtual ~TestSet()
+	{
+		
+	}
+	
+	virtual void SetUp()
+	{
+		
+	}
+	
+	virtual void TearDown()
+	{
+		
+	}
+};
+
+TEST_F(TestSet, NewSetIsEmpty)
+{
+	Set set;
+	EXPECT_EQ(true, set.isEmpty());
+}
+
+TEST_F(TestSet, SetWithOneEleIsNotEmpty)
+{
+	Set set;
+	set.add(1);
+	EXPECT_EQ(false, set.isEmpty());
+}
+
+TEST_F(TestSet, SetAddOneContainsOne)
+{
+	Set set;
+	set.add(1);
+	EXPECT_EQ(true, set.contains(1));
+}
+
+TEST_F(TestSet, SetAddOneDoesNotContainsTwo)
+{
+	Set set;
+	set.add(1);
+	EXPECT_EQ(false, set.contains(2));
+}
+
+TEST_F(TestSet, SetWithOneEleHasSizeOfOne)
+{
+	Set set;
+	set.add(1);
+	EXPECT_EQ(1, set.size());
+}
+
+TEST_F(TestSet, SetWithTwoDistinctEleHasSizeOfTwo)
+{
+	Set set;
+	set.add(1);
+	set.add(2);
+	EXPECT_EQ(2, set.size());
+}
+
+TEST_F(TestSet, SetWithTwoEqualEleHasSizeOfOne)
+{
+	Set set;
+	set.add(1);
+	set.add(1);
+	EXPECT_EQ(1, set.size());
+}
+
+TEST_F(TestSet, SetWithOneEleRemovedOneIsEmpty)
+{
+	Set set;
+	set.add(1);
+	set.remove(1);
+	EXPECT_EQ(0, set.size());
+	EXPECT_EQ(true, set.isEmpty());
+}
+
+TEST_F(TestSet, SetWithOneEleRemovedZeroIsNotEmpty)
+{
+	Set set;
+	set.add(1);
+	set.remove(0);
+	EXPECT_EQ(1, set.size());
+	EXPECT_EQ(false, set.isEmpty());
+}
+
+TEST_F(TestSet, SetWithTwoDistinctEleRemovedOneIsNotEmpty)
+{
+	Set set;
+	set.add(1);
+	set.add(2);
+	EXPECT_EQ(2, set.size());
+	EXPECT_EQ(false, set.isEmpty());
+	set.remove(1);
+	EXPECT_EQ(1, set.size());
+	EXPECT_EQ(false, set.isEmpty());
+}
+
+TEST_F(TestSet, SetWithTwoEqualEleRemovedOneIsEmpty)
+{
+	Set set;
+	set.add(1);
+	set.add(1);
+	EXPECT_EQ(1, set.size());
+	EXPECT_EQ(false, set.isEmpty());
+	set.remove(1);
+	EXPECT_EQ(0, set.size());
+	EXPECT_EQ(true, set.isEmpty());
+}
