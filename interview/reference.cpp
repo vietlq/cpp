754c55cfcfa77464342558d8d9cbfea1e632183b
Le Quoc Viet <vietlq85@gmail.com>
2013-08-17 13:22:23 +0800

    --

---
+++ interview/reference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Free-standing reference not supported
+	//int & b = 12;
+	//std::cout << "b = " << b << std::endl;
+	
+	return 0;
+}
+
